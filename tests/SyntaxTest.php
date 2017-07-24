6f65b33977f9c2d09d48b551bac040851d721d4b
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-07-24 23:55:06 +0200

    Check syntax of each file in src directory

---
+++ tests/SyntaxTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Awesomite\Chariot;
+
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * @internal
+ */
+class SyntaxTest extends TestBase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSyntax()
+    {
+        $counter = 0;
+        foreach ($this->findFiles('src', '*.php') as $file) {
+            require_once $file->getRealPath();
+            $counter++;
+        }
+        $this->assertGreaterThan(0, $counter);
+    }
+
+    /**
+     * @param string $path
+     * @param string $pattern
+     *
+     * @return SplFileInfo[]|\Traversable
+     */
+    private function findFiles(string $path, string $pattern)
+    {
+        $delimiter = DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR;
+        $explodedPath = explode($delimiter, __FILE__);
+        array_pop($explodedPath);
+        $realPath = implode($delimiter, $explodedPath) . DIRECTORY_SEPARATOR . $path;
+
+        return (new Finder())
+            ->in($realPath)
+            ->name($pattern);
+    }
+}
