59b9c8128e4af2269774079542d4cb00671bd9d8
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-08-21 20:00:05 +0200

    Added Awesomite\Chariot\Pattern\StdPatterns\ListPattern

---
+++ src/Pattern/StdPatterns/ListPattern.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Awesomite\Chariot\Pattern\StdPatterns;
+
+class ListPattern extends AbstractPattern
+{
+    public function getRegex(): string
+    {
+        return '[^/]+(,[^/]+)*';
+    }
+
+    public function toUrl($data): string
+    {
+        $normalized = $this->normalizeToUrl($data);
+
+        if (is_string($normalized)) {
+            if (!$this->match($normalized)) {
+                throw $this->newInvalidToUrl($data);
+            }
+
+            return $normalized;
+        }
+
+        if (is_array($normalized) && !empty($normalized)) {
+            return implode(',', $normalized);
+        }
+
+        throw $this->newInvalidToUrl($data);
+    }
+
+    public function fromUrl(string $param)
+    {
+        return explode(',', $param);
+    }
+
+    private function normalizeToUrl($data)
+    {
+        if (is_object($data)) {
+            if ($data instanceof \Traversable) {
+                return iterator_to_array($data);
+            }
+
+            if (method_exists($data, '__toString')) {
+                return (string) $data;
+            }
+        }
+
+        if (is_array($data)) {
+            return $data;
+        }
+
+        if (is_scalar($data)) {
+            return (string) $data;
+        }
+
+        throw $this->newInvalidToUrl($data);
+    }
+}
---
+++ tests/Pattern/StdPatterns/ListPatternTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Awesomite\Chariot\Pattern\StdPatterns;
+
+use Awesomite\Chariot\Exceptions\PatternException;
+use Awesomite\Chariot\Pattern\StringableObject;
+use Awesomite\Chariot\TestBase;
+
+/**
+ * @internal
+ */
+class ListPatternTest extends TestBase
+{
+    /**
+     * @dataProvider providerInvalidToUrl
+     *
+     * @param      $list
+     * @param bool $isInvalid
+     */
+    public function testInvalidToUrl($list, bool $isInvalid)
+    {
+        $pattern = new ListPattern();
+
+        if ($isInvalid) {
+            $this->expectException(PatternException::class);
+            $this->expectExceptionCode(PatternException::CODE_TO_URL);
+        }
+
+        $pattern->toUrl($list);
+    }
+
+    public function providerInvalidToUrl()
+    {
+        return [
+            'int' => [1, false],
+            \stdClass::class => [new \stdClass(), true],
+            'resource' => [tmpfile(), true],
+            'emptyArray' => [[], true],
+            'emptyTraversable' => [new \ArrayObject(), true],
+            'array' => [['foo'], false],
+            'traversable' => [new \ArrayObject(['foo']), false],
+            'invalidStringable' => [new StringableObject(''), true],
+            'stringable' => [new StringableObject('x,y,z'), false],
+            'invalidString' => ['', true],
+            'string' => ['foo', false],
+        ];
+    }
+
+    /**
+     * @dataProvider providerToUrl
+     *
+     * @param             $data
+     * @param string      $expected
+     */
+    public function testToUrl($data, string $expected)
+    {
+        $this->assertSame($expected, (new ListPattern())->toUrl($data));
+    }
+
+    public function providerToUrl()
+    {
+        yield [['foo', 'bar'], 'foo,bar'];
+        yield [['foo'], 'foo'];
+        yield [['a','b','c'], 'a,b,c'];
+        yield [new \ArrayObject(['x', 'y', 'z']), 'x,y,z'];
+        yield ['foo', 'foo'];
+        yield [5, '5'];
+    }
+
+    /**
+     * @dataProvider providerFromUrl
+     *
+     * @param string $urlPart
+     * @param array  $expected
+     */
+    public function testFromUrl(string $urlPart, array $expected)
+    {
+        $this->assertSame($expected, (new ListPattern())->fromUrl($urlPart));
+    }
+
+    public function providerFromUrl()
+    {
+        return [
+            ['a,b,c', ['a', 'b', 'c']],
+            ['hello-world', ['hello-world']],
+        ];
+    }
+}
