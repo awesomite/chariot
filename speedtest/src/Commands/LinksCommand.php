d7bd4520caca46facb0f1086350582bf12d60743
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-09-24 18:03:36 +0200

    Updated exception message

--- speedtest/src/Commands/LinksCommand.php
+++ speedtest/src/Commands/LinksCommand.php
@@ -50,7 +50,7 @@ class LinksCommand extends Command
     private function checkXdebug()
     {
         if (extension_loaded('xdebug')) {
-            throw new \RuntimeException('Do not execute performance tests with enabled xdebug');
+            throw new \RuntimeException('Do not execute performance tests with enabled xdebug (add -n option)');
         }
     }
 
