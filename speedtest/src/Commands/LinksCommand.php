e99b7093c7d6e984bfd5bec8b2a14795594411e6
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-09-24 18:06:31 +0200

    Automatically detect unused imports

--- speedtest/src/Commands/LinksCommand.php
+++ speedtest/src/Commands/LinksCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\Input;
 
 /**
  * @internal
