898fa5f3827ed8042412a0c9ae44fea0d57ce857
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-03-19 22:31:06 +0100

    removed travis_retry

--- bin/infection.sh
+++ bin/infection.sh
@@ -6,10 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if php -r "var_export(get_loaded_extensions(true));" | grep --quiet -i xdebug; then
   cd ${DIR}/..
-  travis_retry wget https://github.com/infection/infection/releases/download/0.8.1/infection.phar -q
-  travis_retry wget https://github.com/infection/infection/releases/download/0.8.1/infection.phar.pubkey -q
+  wget https://github.com/infection/infection/releases/download/0.8.1/infection.phar -q
+  wget https://github.com/infection/infection/releases/download/0.8.1/infection.phar.pubkey -q
   chmod +x infection.phar
-  travis_retry ./infection.phar self-update
+  ./infection.phar self-update
   ./infection.phar
 else
   echo "Xdebug is not installed"
