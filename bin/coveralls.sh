edbcd0c55ecc9b82e01079379ed9eb7b33620e01
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-03-19 22:07:57 +0100

    * removed humbug * added infection

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if php -r "var_export(get_loaded_extensions(true));" | grep --quiet -i xdebug; then
-  php ${DIR}/../vendor/bin/coveralls -v
+  php ${DIR}/../dev-tools/vendor/bin/php-coveralls -v
 else
   echo "Xdebug is not installed"
 fi
